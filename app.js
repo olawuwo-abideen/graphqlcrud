cda4b98b63bb16600ee36f25e02f6ac98fb5ba8d
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2024-03-26 01:44:30 +0100

    update

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
-var {graphql, buildSchema} = require("graphql")
+const {graphql, buildSchema} = require("graphql")
+const express = require("express")
+const {createHandler}  = require('graphql-http/lib/use/express')
+var {ruruHTML} = require('ruru/server')
 
-var schema = buildSchema(
+const schema = buildSchema(
     ` type Query {
         hello: String
         age: Int
+        year: Int
+        ysd : Float
     }   
 `)
 
-var rootValue = {
+const rootValue = {
     hello:() => {
         return "Hello world"
     },
     age:() => {
         return 25;
     },
+    year:() => {
+        return 1997;
+    },
+    ysd:() => {
+        return 19.97;
+    },
 }
 
 
-graphql({
-    schema,
-    source: "{age}",
-    rootValue,
-}).then(response => {
-    console.log(response)
+const app = express();
+
+app.all('/graphql' , createHandler({schema, rootValue}));
+
+app.get('/' , (_req, res) => {
+    res.type('html')
+    res.end(ruruHTML({endpoint: '/graphql'}))
 })
+
+
+app.listen(3000)
+console.log(`
+App listening on port 3000
+Test: http://localhost:3000/graphql?query={hello, age}
+
+`)
