e3e0b51351f2bf2aaf45b43501d608652bbcf37c
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2024-03-27 01:03:21 +0100

    update package

--- app.js
+++ app.js
@@ -10,81 +10,8 @@ const express = require("express")
 const {createHandler}  = require('graphql-http/lib/use/express')
 var {ruruHTML} = require('ruru/server')
 
-// const schema = buildSchema(
-//     ` type Query {
-//         hello(name: String!): String
-//         age: Int
-//         year: Int
-//         ysd : Float!
-//         present: Boolean
-//         Hobbies: [String!]!
-//         user:User
-//     }   
-//     type User{
-//         id: Int
-//         name: String
-//     }
-// `)
-
-const User = new GraphQLObjectType({
-        name:'User',
-        fields: {
-            id: {
-                type: GraphQLInt,
-            },
-              name: {
-                type: GraphQLString,
-                resolve: (obj) => {
-                    const name = obj.name.trim().toUpperCase();
-                    if (obj.isAdmin) {
-                        return `${name} (Admin)`;
-                    }
-                    return name
-                }
-              }
-        }
-    })
 
 
-
-const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name:'Query',
-        fields: {
-            hello: {
-                type: GraphQLString,
-                resolve: () => {
-                    return "Hello world";
-                }
-            }
-        }
-    })
-})
-
-
-// const rootValue = {
-//     hello:({name}) => {
-//         return "Hello " + name
-//     },
-//     age:() => {
-//         return 25;
-//     },
-//     year:() => {
-//         return 1997;
-//     },
-//     ysd: 19.97,
-//     present:true,
-//     Hobbies:() => {
-//         return ["eating", "swimming"]
-//     },
-
-// user:() => {
-//     return {
-//         id: 1,
-//         name:'wale'
-//     } ;
-// },
-// };
 
 
 const app = express();
