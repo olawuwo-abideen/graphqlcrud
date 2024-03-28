2879f82840c277d5cca5cbf617f0933d5b2ef368
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2024-03-28 13:16:30 +0100

    update models

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,22 +1,25 @@
-import { createSchema} from 'graphql-yoga'
-import {typeDef as User, resolvers as userResolvers} from './models/user.js'
+import { createSchema } from 'graphql-yoga';
+import { typeDef as User, resolvers as userResolvers } from './models/user.js';
+import {
+  typeDef as Comment,
+  resolvers as commentResolvers,
+} from './models/comments.js';
+
 import _ from 'lodash';
- 
+
 const queries = /* GraphQL */ `
-        type Query {
-          hello: String
-        }
+  type Query {
+    hello: String
+  }
 `;
 
-
 const resolvers = {
-    Query: {
-        hello: () => 'Hello from Yoga!',
-    },
-}
+  Query: {
+    hello: () => 'Hello from Yoga!',
+  },
+};
 
 export const schema = createSchema({
-    typeDefs: [queries, User],
-    resolvers: _.merge(resolvers, userResolvers) 
-})
-     
+  typeDefs: [queries, User, Comment],
+  resolvers: _.merge(resolvers, userResolvers, commentResolvers),
+});
--- src/graphql/models/user.js
+++ src/graphql/models/user.js
@@ -1,43 +1,75 @@
-export const typeDef  = /* GraphQL */`
-        type Query {
-          user: User
-        }
-        type Mutation {
-            createUser:(user:NewUserInput!): User
-        }
-        type User{
-            id: Int
-            name: String
-            age: Int
-        } 
-        input NewUserInputUser{
-            name: String!
-            age: int!
-        } 
-      `;
+import { ObjectId } from 'mongodb';
 
-export  const resolvers = {
-    Query:{
-     user:  () => {
-         return {
-             id: 1,
-             name: 'wale',
-     };
- },
- },
- Mutation: {
-    createUser: ({user}) => {
-       return {
-        id:1,
-        ...user
-       };
-    }
- }, 
- 
- User: {
-     name: (obj) => {
-         return obj.name.toUpperCase()
-     }
- }
-     
- }
+export const typeDef = /* GraphQL */ `
+  type Query {
+    users: [User!]!
+    user(id: ID!): User
+  }
+
+  type Mutation {
+    createUser(user: NewUserInput!): User
+    deleteUser(id: ID!): Boolean
+    updateUser(id: ID!, update: UpdateUserInput): User
+  }
+
+  input NewUserInput {
+    name: String!
+    email: String!
+  }
+
+  input UpdateUserInput {
+    name: String!
+  }
+
+  type User {
+    id: ID!
+    name: String
+    email: String
+
+    comments: [Comment]
+  }
+`;
+
+export const resolvers = {
+  Query: {
+    users: (obj, args, { mongo }) => {
+      return mongo.users.find().limit(20).toArray();
+    },
+
+    user: async (obj, { id }, { mongo }) => {
+      return mongo.users.findOne({ _id: new ObjectId(id) });
+    },
+  },
+
+  Mutation: {
+    createUser: async (_, { user }, { mongo }) => {
+      const response = await mongo.users.insertOne(user);
+      return {
+        id: response.insertedId,
+        ...user,
+      };
+    },
+    deleteUser: async (obj, { id }, { mongo }) => {
+      await mongo.users.deleteOne({ _id: new ObjectId(id) });
+      return true;
+    },
+    updateUser: async (obj, { id, update }, { mongo }) => {
+      console.log(update);
+      const response = await mongo.users.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { name: update.name } }
+      );
+      return mongo.users.findOne({ _id: new ObjectId(id) });
+    },
+  },
+
+  User: {
+    id: ({ id, _id }) => _id || id,
+    name: (obj) => {
+      return obj.name.trim().toUpperCase();
+    },
+    comments: ({ email }, args, { mongo }) => {
+      return mongo.comments.find({ email }).limit(20).toArray();
+    },
+  },
+};
